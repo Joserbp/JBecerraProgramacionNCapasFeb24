fb81e11cfc91c74cbc3701cf971edca69ae0406b
Joserbp <josera.jack@gmail.com>
2024-03-19 12:00:25 -0600

    WEBAPI

---
+++ SL_WebApi/Controllers/OperacionesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SL_WebApi.Controllers
+{
+    public class OperacionesController : ApiController
+    {
+        //Investigacion
+        //Que es WebServices
+        //Que es REST
+        //       SOAP
+        // Como se consumen
+        //DataContract, DataMember, OperationCOntract y ServiceContract
+        //Verbos HTTP
+        //Status Code Http
+        //Endpoint
+
+        //En un word con capturas del codigo + capturas de postman como se consumio
+        //WPS o drive
+        //Se manda personal
+
+
+        //Endpoint -- Rutas
+        [Route("api/Saludo")]
+        [HttpGet]
+        public IHttpActionResult Saludar(string Nombre)
+        {
+            string saludo = "Hola " + Nombre;
+            //Tipo de retorno JSON, HTTP Status Code 100,200,300 Grupos
+            return Content(HttpStatusCode.OK,saludo);
+        }
+        [Route("api/Suma")]
+        [HttpGet]
+        public IHttpActionResult Suma(int numero1 , int numero2)
+        {
+            string saludo = "Hola "
+            //Tipo de retorno JSON, HTTP Status Code 100,200,300 Grupos
+            return Content(HttpStatusCode.OK, saludo);
+        }
+    }
+}
